2d73dfa7d7c8af16557aa0010484ef08e1fa132f
EduardOrellana <ore23006@byui.edu>
2024-12-13 20:40:51 -0600

    fixing bugs

--- project_eeou/js/project.js
+++ project_eeou/js/project.js
@@ -2,41 +2,37 @@ import utility from "./utilities.mjs";
 import fetchMovie from "./Source.mjs";
 import { templateTopMovie } from "./Source.mjs";
 
-document.addEventListener("DOMContentLoaded", async () => {
-    // load header
-    await utility.loadHeader();
-    utility.hambutton();
-    utility.loadFooter();
+await utility.loadHeader();
+utility.hambutton();
+utility.loadFooter();
 
-    let movieInfo;
+let movieInfo;
 
-    document.querySelector("#seek").addEventListener("click", async () => {
+document.querySelector("#seek").addEventListener("click", async () => {
 
-        const movie = document.querySelector("#movie").value.trim();
+    const movie = document.querySelector("#movie").value.trim();
 
 
-        //verify the input
-        if (movie === "") {
-            document.querySelector("#poster").innerHTML = "<p>Please try again.</p>";
-            return;
-        }
+    //verify the input
+    if (movie === "") {
+        document.querySelector("#poster").innerHTML = "<p>Please try again.</p>";
+        return;
+    }
 
-        movieInfo = await fetchMovie(movie);
+    movieInfo = await fetchMovie(movie);
 
-        document.querySelector("#poster").innerHTML = templateTopMovie(movieInfo);
+    document.querySelector("#poster").innerHTML = templateTopMovie(movieInfo);
 
-        return movieInfo
-    });
-
-
-    const buttonFavorites = document.querySelector("#addCatalog")
-
-    buttonFavorites.addEventListener("click", () => {
-        console.log(movieInfo);
-        utility.setLocalStorageJSON("favorites", movieInfo)
-    })
-
+    return movieInfo
 });
+
+
+const buttonFavorites = document.querySelector("#addCatalog")
+
+buttonFavorites.addEventListener("click", () => {
+    console.log(movieInfo);
+    utility.setLocalStorageJSON("favorites", movieInfo)
+})
 
 
 
